b6c1de7834d10e2085f0abe8313ef4c642f0cd56
Anowar1987 <anowarhossen.iim@gmail.com>
2024-04-04 23:27:38 +0600

    header page completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,24 @@
 import Header from "../Shared/Header/Header";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import NavBar from "../Shared/NavBar/NavBar";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
             <NavBar></NavBar>
-            <h2 className="text-3xl font-poppins font-bold">This is Home</h2>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                <div className="">
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="md:col-span-2">
+                    <h2 className="text-3xl">News Coming Soon</h2>
+                </div>
+                <div className="">
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,12 @@
-
+import logo from '../../../assets/Images/logo.png';
+import moment from 'moment';
 
 const Header = () => {
     return (
-        <div>
-            <h2 className="text-3xl">This is Header</h2>
+        <div className='text-center mt-12'>
+            <img src={logo} alt="" />
+            <p>Journalism Without Fear or Favour</p>
+            <p>{moment().format("dddd, MMMM D, YYYY")}</p>
         </div>
     );
 };
